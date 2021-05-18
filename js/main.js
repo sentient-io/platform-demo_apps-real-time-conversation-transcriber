1e787e10f086211807feb51936e944ffe25030d8
Priyadharshini <priyadharshini@sentient.io>
2021-05-18 10:24:22 +0530

    latest files

--- js/main.js
+++ js/main.js
@@ -1,139 +1,148 @@
+/** List of HTML element been selected by ID for easy access later */
+const domElem = {
+    /**
+     * Each id in html page should be registered here
+     * Access the html element by domElem.(html id text) or domElem[html id text]
+     * */
+    modelSelector: document.getElementById('modelSelector'),
+    modelSelectorSection: document.getElementById('modelSelectorSection'),
+    modelDescription: document.getElementById('modelDescription'),
+    startRecordingSection: document.getElementById('startRecordingSection'),
+    transcriptSection: document.getElementById('transcriptSection'),
+    transcribeResults: document.getElementById('transcribeResults'),
+    pauseButton: document.getElementById('pauseButton'),
+    resumeButton: document.getElementById('resumeButton'),
+    loader: document.getElementById('loader'),
+
+    hideById: function (elems) {
+        elems
+            .replace(/\s/g, '')
+            .split(',')
+            .forEach((elem) => {
+                this[elem].style.display = 'none';
+            });
+    },
+    flexById: function (elems) {
+        /** Set the display value of dom id as flex */
+        elems
+            .replace(/\s/g, '')
+            .split(',')
+            .forEach((elem) => {
+                this[elem].style.display = 'flex';
+            });
+    },
+    showById: function (elems) {
+        /** Set the display value of dom id as flex */
+        elems
+            .replace(/\s/g, '')
+            .split(',')
+            .forEach((elem) => {
+                this[elem].style.display = 'block';
+            });
+    },
+};
+
+/** Global object to store state and data */
 const state = {
-	seqNum: 1,
-	partial: '',
+    cliApiKey: '',
+    model: 'en-SG',
+    sampleRate: 16000,
+    seqNum: 1,
+    modelDescription: {
+        'en-SG':
+            'Better suited for transcribing prepared / formal speech (e.g. news or parliamentary speeches) with the Singaporean accent.',
+        'en-SG-conversation':
+            'Better suited for transcribing casual speech with the Singaporean accent. Tuned to also recognise names of local food and places.',
+        'en-SG-telephony':
+            'Better suited for transcribing conversations in lower sampling rate (e.g. 8khz for telephony).',
+        generic_en_vosk:
+            'Better suited for generic / broad usage across different accents.',
+    },
 };
 
-// Full page loader
-const pageLoader = {
-	loader: document.getElementById('pageLoader'),
-	show: function show() {
-		this.loader.style = 'display:block';
-	},
-	hide: function hide() {
-		this.loader.style = 'display:none';
-	},
+const userSelections = {
+    model: function () {
+        const model = domElem.modelSelector.value;
+        state.model = model;
+
+        domElem.modelDescription.innerHTML = state.modelDescription[model];
+        console.log('Model selected: ', model);
+    },
+};
+/** Listen to dropdown list selection event */
+domElem.modelSelector.addEventListener('input', userSelections.model);
+
+/** Timer for sending data to websocket */
+let send_interval;
+
+const userActions = {
+    startTranscribe: function () {
+        Recorder.get(function (rec) {
+            recorder = rec;
+
+            recorder.start();
+        })
+            .then(() => {
+                loader.start();
+                return startNewWebsocket();
+            })
+
+            .then(() => {
+                domElem.hideById('modelSelectorSection, startRecordingSection');
+                domElem.showById('transcriptSection');
+                // Start the interval timer, send audio blob to websocket every 1 second
+                send_interval = setInterval(() => {
+                    // Send audio blob to websocket
+                    webSocket.send(recorder.getBlob());
+                    // When start called, old audio data will be cleared
+                    recorder.start();
+                }, 500);
+            })
+            .catch((err) => {
+                if (JSON.parse(err).message.includes('Unauthorized User')) {
+                    state.cliApiKey = prompt(
+                        'Unauthorized user, to continue test out, please provide a valid API key and try again.',
+                        state.cliApiKey
+                    );
+                } else {
+                    alert(err);
+                }
+            })
+            .finally(() => {
+                loader.stop();
+            });
+    },
+    restart: function () {
+        location.reload();
+    },
+    pauseTranscribe: function () {
+        // Stop interval for sending audio blob to websocket
+        clearInterval(send_interval);
+        domElem.hideById('pauseButton');
+        domElem.flexById('resumeButton');
+    },
+    resumeTranscribe: function () {
+        domElem.flexById('pauseButton');
+        domElem.hideById('resumeButton');
+        send_interval = setInterval(() => {
+            // Send audio blob to websocket
+            webSocket.send(recorder.getBlob());
+            // When start called, old audio data will be cleared
+            recorder.start();
+        }, 500);
+    },
+    clearText: function () {
+        domElem.transcribeResults.innerText = '';
+    },
 };
 
-// Helper function change selected elemnt's style="display:" value
-elementDisplay = function elementDisplay(ID) {
-	return {
-		idArr: ID.replace(/\#/g, '').split(' '),
-		block: function block() {
-			this.idArr.forEach((id) => {
-				elem = document.getElementById(id);
-				elem.style.display = 'block';
-			});
-		},
-		flex: function flex() {
-			this.idArr.forEach((id) => {
-				elem = document.getElementById(id);
-				elem.style.display = 'flex';
-			});
-		},
-		hide: function none() {
-			this.idArr.forEach((id) => {
-				elem = document.getElementById(id);
-				elem.style.display = 'none';
-			});
-		},
-	};
-};
+const utility = {};
 
-// Full page popup
-const pagePopup = {
-	popup: document.getElementById('pagePopup'),
-	title: document.getElementById('pagePopupTitle'),
-	content: document.getElementById('pagePopupContent'),
-	open: function open(config) {
-		this.title.innerHTML = config.title;
-		this.content.innerHTML = config.content;
-		this.popup.style = 'display:flex';
-	},
-	close: function close() {
-		this.title.innerHTML = '';
-		this.content.innerHTML = '';
-		this.popup.style = 'display:none';
-	},
-};
-
-// pagePopup.open({ title: 'test title', content: 'test content' });
-
-// Show loader
-pageLoader.show();
-// Calling new websocket when page loaded
-startNewWebsocket();
-
-// Interval timer, sending audio blob to web socket
-let send_interval = null;
-
-startRecording = function startRecording() {
-	// Get user's permission of mic usage and start record aduio
-	Recorder.get(function (rec) {
-		recorder = rec;
-		// console.log('Start recording');
-		// Start recording
-		recorder.start();
-
-		// Hide [Start Transcribe] button and display [Stop Transcribe] button
-		// document.getElementById('btnStart').style = 'display:none';
-		elementDisplay('#btnStart #btnContinue #resultContainerNotes').hide();
-		elementDisplay('#btnPause #btnStop').flex();
-
-		// Start the interval timer, send audio blob to websocket every 1 second
-		send_interval = setInterval(() => {
-			// Send audio blob to websocket
-			webSocket.send(recorder.getBlob());
-			// When start called, old audio data will be cleared
-			recorder.start();
-		}, 1000);
-	});
-
-	// This function will NOT clean the existing transcribe result
-};
-
-stopRecording = function stopRecording() {
-	// console.log('Stop recording');
-
-	// Stop interval for sending audio blob to websocket
-	clearInterval(send_interval);
-	// Send empty element to indicate end of stream
-	webSocket.send('');
-
-	// Show [Restart] button and hide [Stop Transcribe] button
-	elementDisplay('#btnRestart').flex();
-	elementDisplay('#btnContinue #btnPause #btnStop').hide();
-};
-
-pauseRecording = function pauseRecording() {
-	// Pause recording, but maintain websocket connection
-	// console.log('Stop recording');
-	// Stop interval for sending audio blob to websocket
-	clearInterval(send_interval);
-
-	// Show [Continue Transcription] button, hide [Pause] button
-	elementDisplay('#btnContinue').flex();
-	elementDisplay('#btnPause').hide();
-};
-
-restart = function restart() {
-	// Show loader
-	pageLoader.show();
-	// Create new webSocket
-	startNewWebsocket();
-
-	// Clean up transcribe result
-	clearTranscribe();
-
-	// Hide [Restart Button] and show [Start Transcribe] button
-	elementDisplay('#btnStart').flex();
-	elementDisplay('#resultContainerNotes').block();
-	elementDisplay('#btnRestart').hide();
-
-	// Initialize state
-	state.seqNum = 1;
-	state.partial = '';
-};
-
-clearTranscribe = function clearTranscribe() {
-	document.getElementById('transcribeResults').innerText = '';
+const loader = {
+    start: function () {
+        domElem.flexById('loader');
+    },
+    stop: function () {
+        domElem.hideById('loader');
+    },
 };
